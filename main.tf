0c022669392f384157818ed082257859850fbf4f
izziedelph <ham.isma@yahoo.com>
2024-09-23 00:49:05 +0100

    Autoscalling group added for high availability

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "CCFS" {
     }
 }
 
+
 resource "aws_security_group" "CCFS-instance" {
 
   name = "Crawley-Carribean-Food-Service-instance"
@@ -28,6 +29,52 @@ resource "aws_security_group" "CCFS-instance" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_launch_configuration" "CCFS" {
+  image_id        = "ami-0e86e20dae9224db8"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.CCFS-instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello our lovlies, Welcome to your favourite island food stop CCFS" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_autoscaling_group" "CCFS" {
+  launch_configuration = aws_launch_configuration.CCFS.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+ 
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-CCFS"
+    propagate_at_launch = true
+  }
+}
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
