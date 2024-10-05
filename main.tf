270d1d528f56b7469adbb6fbcf29c74e18709c62
izziedelph <ham.isma@yahoo.com>
2024-10-05 18:27:39 +0100

    Database added to architecture with s3 configured as backend and state storage

--- main.tf
+++ main.tf
@@ -58,9 +58,9 @@ resource "aws_autoscaling_group" "CCFS" {
   health_check_type = "ELB"
  
 
-  min_size         = 0 # Ensure no instances are running
+  min_size         = 2 # Ensure no instances are running
   max_size         = 10
-  desired_capacity = 0  # Force no instances to be running
+  #desired_capacity = 0  # Force no instances to be running
 
   tag {
     key                 = "Name"
@@ -173,4 +173,108 @@ resource "aws_lb_listener_rule" "CCFS" {
 output "alb_dns_name" {
   value       = aws_lb.CCFS.dns_name
   description = "The domain name of the load balancer"
+}
+
+
+resource "aws_s3_bucket" "CCFS_state" {
+
+  bucket = "ccfs-state-bucket"
+
+  lifecycle {
+  prevent_destroy = false
+  }
+}
+
+
+# Enable versioning so you can see the full revision history of your state files
+resource "aws_s3_bucket_versioning" "CCFS-enabled" {
+  bucket = aws_s3_bucket.CCFS_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "CCFS-default" {
+  bucket = aws_s3_bucket.CCFS_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.CCFS_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_dynamodb_table" "CCFS-terraform_locks" {
+  name         = "CCFS-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket         = "ccfs-state-bucket"       
+    key            = "global/s3/terraform.tfstate"  
+    region         = "us-east-1"               
+    dynamodb_table = "CCFS-locks"   
+    encrypt        = true                        
+  }
+}
+
+
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.CCFS_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.CCFS-terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
+
+resource "aws_db_instance" "CCFS-db" {
+  identifier_prefix   = "ccfs-example"
+  engine              = "mysql"
+  allocated_storage   = 20
+  engine_version      = "8.0.35"
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+
+  db_name             = var.db_name
+
+  username = var.db_username
+  password = var.db_password
+}
+
+
+output "address" {
+  value       = aws_db_instance.CCFS-db.address
+  description = "Connect to the database at this endpoint"
+}
+
+
+output "port" {
+  value       = aws_db_instance.CCFS-db.port
+  description = "The port the database is listening on"
 }
